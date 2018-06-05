f5436170f1690a79d9f5f15e27588951bfd2c129
laurent carpio <laurent.carpio@gmail.com>
2018-06-05 15:46:37 -0400

    new branch test

---
+++ test/newFileFortest.swift
@@ -0,0 +1,12 @@
+//
+//  newFileFortest.swift
+//  test
+//
+//  Created by Laurent Carpio on 18-06-05.
+//  Copyright © 2018 Laurent Carpio. All rights reserved.
+//
+
+import Foundation
+
+struct newFileFortest {
+}
